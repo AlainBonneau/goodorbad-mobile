ff534f08a1feecb7e34f442767d82fa8613ca456
Alain Bonneau <alaindev97@gmail.com>
2025-08-23 04:08:51 +0200

    configure expo router and light mode

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -3,6 +3,7 @@ module.exports = function (api) {
   return {
     presets: [["babel-preset-expo", { jsxImportSource: "nativewind" }]],
     plugins: [
+      "expo-router/babel",
       "react-native-reanimated/plugin", // gardez celui-ci en dernier
     ],
   };
